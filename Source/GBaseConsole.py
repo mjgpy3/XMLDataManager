202bf5991d97300cc85b5bd30f0432bd999431e3
Michael Gilliland <mjg.py3@gmail.com>
2012-05-17 14:44:24 -0400

    Made the output a little bit nicer and added some test data

--- Source/GBaseConsole.py
+++ Source/GBaseConsole.py
@@ -11,6 +11,7 @@ from GBaseCommandString import GBaseCommandString as gbc
 commandString = gbc()
 handler = gbh()
 errorOccured = False
+longestResult = 0
 
 while True:
 	command = raw_input('gbase> ')
@@ -28,15 +29,20 @@ while True:
 		try:
 			if commandString.Command == 'use':
 				handler.Use(commandString.GBaseName)
+				print "Now using " + commandString.GBaseName
 			elif commandString.Command == 'gen':
 				if commandString.ActsUpon == 'gbase':
 					handler.GenGBase(commandString.GBaseName)
+					print 'Generated GBase "' + commandString.GBaseName + '"'
 				elif commandString.ActsUpon == 'table':
 					handler.GenTable(commandString.TableName)
+					print 'Generated Table "' + commandString.TableName + '"'
 				elif commandString.ActsUpon == 'col':
 					handler.GenColIn(commandString.ColName, "string", commandString.TableName)
+					print 'Generated Column "' + commandString.ColName + '"'
 				elif commandString.ActsUpon == 'row':
 					handler.GenRowIn(commandString.WithAttributes, commandString.WithValues, commandString.TableName)
+					print 'Generated Row with ' + str(commandString.WithValues) + ' in ' + commandString.TableName
 			elif commandString.Command == 'del':
 				if commandString.ActsUpon == 'gbase':
 					handler.DelGBase(commandString.GBaseName)
@@ -46,17 +52,28 @@ while True:
 					handler.DelColName(commandString.ColName, commandString.TableName)
 				elif commandString.ActsUpon == 'row':
 					handler.DelRowWith(commandString.WithAttributes, commandString.WithValues, commandString.TableName)
+				print 'Deleted ' + commandString.ActsUpon
 			elif commandString.Command == 'get':
 				result = handler.GetRowWith(commandString.WithAttributes, commandString.WithValues, commandString.TableName)
+				print 'Query Result:'
+
 				for tuple in result:
 					for datum in tuple:
-						print "{:<30}".format(datum),
+						if longestResult < len(datum):
+							longestResult = len(datum)
+
+				for tuple in result:
+					for datum in tuple:
+						print ("{:<" + str(longestResult) + "}").format(datum),
 					print ''
 			elif commandString.Command == 'mod':
 				handler.ModRowWith(commandString.WithAttributes, commandString.WithValues, commandString.TableName, commandString.ThatAttributes, commandString.ThatValues)
+				print "Modified row"
 			elif commandString.Command == 'help':
 				print "Help not yet implemented"
 		except Exception as e:
 			print e
 
+	print ''
 	errorOccured = False
+	longestResult = 0
